fba20d8cb1b04ca09972bbdd91ea50e6c3774f54
Shivansh Singh <singhshivansh244@gmail.com>
2022-04-12 12:13:51 +0530

    Rat in maze

--- Recursion/Rat_in_a_Maze_Problem_I_gfg/Rat_in_a_Maze_Problem_I_gfg.cpp
+++ Recursion/Rat_in_a_Maze_Problem_I_gfg/Rat_in_a_Maze_Problem_I_gfg.cpp
@@ -1,31 +1,46 @@
-#include </Users/shivanshsingh/Desktop/GithubRep/template.h>
+#include "../../template.h"
 
-void helper(vvi &a, int i, int j, string temp, vector<string> &ans) {
-  if (a[i][j] == 0) return;
+void helper(vvi &a, int i, int j, string temp, vector<string> &ans,
+            vector<vector<bool>> vis) {
+  print__2D(vis);
+  cout << nl;
+  if (a[i][j] == 0 or vis[i][j]) return;
+  vis[i][j] = 1;
   if (i == a.size() - 1 and j == a[0].size() - 1) {
     ans.pb(temp);
     return;
   }
+  if (i == 0 and j == a[0].size() - 1) {
+    helper(a, i + 1, j, temp + 'D', ans, vis);
+    return;
+  }
+  if (i == 0) {
+    helper(a, i, j + 1, temp + 'R', ans, vis);
+    return;
+  }
   if (i == a.size() - 1) {
-    helper(a, i, j + 1, temp + 'R', ans);
+    helper(a, i, j + 1, temp + 'R', ans, vis);
     return;
   }
   if (j == a[0].size() - 1) {
-    helper(a, i + 1, j, temp + 'D', ans);
+    helper(a, i + 1, j, temp + 'D', ans, vis);
     return;
   }
-  helper(a, i + 1, j, temp + 'D', ans);
-  helper(a, i, j + 1, temp + 'R', ans);
+  helper(a, i + 1, j, temp + 'D', ans, vis);
+  helper(a, i - 1, j, temp + 'R', ans, vis);
+  helper(a, i, j + 1, temp + 'R', ans, vis);
 }
 
 void solve(vvi &a) {
   vector<string> ans;
-  helper(a, 0, 0, "", ans);
+  vector<vector<bool>> vis(a.size(), vector<bool>(a[0].size(), 0));
+  helper(a, 0, 0, "", ans, vis);
   fo(i, 0, ans.size()) cout << ans[i] << " ";
   return;
 }
 
 int main() {
-  vvi a = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+  vvi a = {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}};
+  // print__2D(a);
   solve(a);
 }
